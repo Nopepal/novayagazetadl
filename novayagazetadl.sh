4ecaf89d974c0078b1d78f8a38d2b80087768abf
Nopepal <83215210+Nopepal@users.noreply.github.com>
2021-10-11 23:33:26 +0700

    Added download options and download dir options
    Added download options: download latest issues from -f X to -t Y, where X is offset and Y is Yth issue counting backwards. Using -d DIR, you can change your download directory.

--- novayagazetadl.sh
+++ novayagazetadl.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
-apiurl='https://novayagazeta.ru/api/v1/get/newspapers'
-ngjson=$(curl -fs "$apiurl")
-for i in $(seq 0 4)
+newspaper_directory="."
+
+fromissue=1
+toissue=31
+while getopts f:t:d: option
 do
-	npurl=$(printf "%s" "$ngjson" | jq ".newspapers|.[${i}]|.pdfFile" | tr -d \")
-	nptitle=$(printf "%s" "$ngjson" | jq ".newspapers|.[${i}]|.title" | tr -d \")
-	wget -nc "$npurl" -O "${nptitle}.pdf"
+        case "${option}"
+                in
+                f) fromissue=${OPTARG};;
+                t) toissue=${OPTARG};;
+                d) newspaper_directory=${OPTARG};;
+        esac
+done
+limit=$(($toissue - $fromissue + 1))
+offset=$(($fromissue - 1))
+[ $offset -lt 0 ] && exit 1
+[ $limit -lt 1 ] && exit 1
+apiurl="https://novayagazeta.ru/api/v1/get/newspapers?limit=${limit}&offset=${offset}"
+ngjson=$(wget -q -O - "$apiurl")
+for i in $(seq 0 $(($limit - 1)))
+do
+        npurl=$(printf "%s" "$ngjson" | jq ".newspapers|.[${i}]|.pdfFile" | tr -d \")
+        nptitle=$(printf "%s" "$ngjson" | jq ".newspapers|.[${i}]|.title" | tr -d \")
+        wget -nc "$npurl" -O "${newspaper_directory}/${nptitle}.pdf"
 done
